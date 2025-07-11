d482140fc6897e28441aca480e7b45bdcf0b3ea7
luftqi <153885428+luftqi@users.noreply.github.com>
2025-07-11 09:19:56 +0800

    Update MQTT_SQLit_Blynk.py

--- MQTT_SQLit_Blynk.py
+++ MQTT_SQLit_Blynk.py
@@ -11,7 +11,7 @@ from os import system
 
 # --- 設定 ---
 # [OTA] 版本號和 GitHub Repo 的設定
-CURRENT_VERSION = 2.0 
+CURRENT_VERSION = 3.0 
 # 請將下面的連結換成您自己 GitHub Repo 中的 "Raw" 連結
 VERSION_URL = "https://raw.githubusercontent.com/luftqi/solar_picow6000/main/pizero_version.txt"
 SCRIPT_URL = "https://raw.githubusercontent.com/luftqi/solar_picow6000/main/MQTT_SQLit_Blynk.py"
